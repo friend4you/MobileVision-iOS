1945d38370e063b9c3e718b4723165885c5ed67b
Vlad Arseniuk <vlad.arsenyuk@gmail.com>
2019-02-09 16:32:01 +0200

    Project initial
    Initial project was added, added main view controller

---
+++ MobileVision/MobileVision/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  MobileVision
+//
+//  Created by Vlad Arsenyuk on 2/9/19.
+//  Copyright © 2019 Arseniuk. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
